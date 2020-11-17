d852aebb52f9246b6db4e7e64321f32d6c52b05b
dunham <dmmcbride@dons.usfca.edu>
2020-11-16 18:18:12 -0800

    Added comments

--- src/Judge.java
+++ src/Judge.java
@@ -2,6 +2,7 @@ public class Judge {
     public int findJudge(int N, int [][] trust){
         int [] town = new int[N + 1];
 
+        // increments trust score for each person that is trusted and decrements for each that trusts
         for (int[] rel : trust)
         {
             town[rel[0]]--;
@@ -10,6 +11,8 @@ public class Judge {
 
         for (int i=1; i<=N; i++)
         {
+            // if someone is trusted by all members of the community and trusts no one else, their trust score will be
+            // equal to one less than the population
             if (town[i]==N-1)
             {
                 return i;
@@ -20,7 +23,7 @@ public class Judge {
 
     public static void main(String [] Args)
     {
-        int[][] arr = { { 1, 3 }, { 2, 3}, {4,3}, {2,4} };
+        int[][] arr = {{1,3}, {2,3}, {4,3}, {2,4}};
         Judge judge = new Judge();
         System.out.println(judge.findJudge(4, arr));
     }
